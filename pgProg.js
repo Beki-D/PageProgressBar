f116ca6b7c5ee6cc11e85d8fe8ebc394651a8682
beki-d <bk14drobs@gmail.com>
2022-10-28 20:43:40 +0300

    init

---
+++ pgProg.js
@@ -0,0 +1,43 @@
+// const handleOnMouseMove = e => {
+//     const { currentTarget: target } = e;
+
+//     const rect = target.getBoundingClientRect(),
+//         x = e.clientX - rect.left,
+//         y = e.clientY - rect.top;
+
+//     target.style.setProperty("--mouse-x", `${x}px`);
+//     target.style.setProperty("--mouse-y", `${y}px`);
+// }
+
+// for (const card of document.getElementsByClassName('card')) {
+//     card.onmousemove = e => handleOnMouseMove(e);
+// }
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")){
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+            
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    }
+}
+
+var body = document.body, html = document.documentElement, progressBar = document.getElementById("progress-bar");
+var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+//Progress bar part
+let totalHeight = document.documentElement.scrollHeight;
+let scrollHeight = document.documentElement.scrollTop;
+console.log(height,"h .", totalHeight,"th .", scrollHeight,"st");
+
+progressBar.style.setProperty("--scroll-total", `${totalHeight}`);
+
+window.onscroll = e => {
+    let scrollHeight = document.documentElement.scrollTop;
+    progressBar.style.setProperty("--scroll-y", `${scrollHeight}`);
+
+    progressBar.style.setProperty("--width", `${(scrollHeight/totalHeight)*100}%`);
+
+}
