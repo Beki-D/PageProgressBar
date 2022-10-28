b1a17d36ed2f339d362b33882250570225072d12
beki-d <bk14drobs@gmail.com>
2022-10-28 22:27:47 +0300

    Removed some zombie code

--- pgProg.js
+++ pgProg.js
@@ -1,18 +1,3 @@
-// const handleOnMouseMove = e => {
-//     const { currentTarget: target } = e;
-
-//     const rect = target.getBoundingClientRect(),
-//         x = e.clientX - rect.left,
-//         y = e.clientY - rect.top;
-
-//     target.style.setProperty("--mouse-x", `${x}px`);
-//     target.style.setProperty("--mouse-y", `${y}px`);
-// }
-
-// for (const card of document.getElementsByClassName('card')) {
-//     card.onmousemove = e => handleOnMouseMove(e);
-// }
-
 document.getElementById("cards").onmousemove = e => {
     for(const card of document.getElementsByClassName("card")){
         const rect = card.getBoundingClientRect(),
@@ -34,4 +19,3 @@ window.onscroll = e => {
 
     progressBar.style.setProperty("--width", `${scrollY / (scrollHeight - innerHeight) *100}%`);
 }
-
